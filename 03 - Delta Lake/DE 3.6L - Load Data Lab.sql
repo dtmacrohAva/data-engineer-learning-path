b124fcc0bf6a5be5cc289c47d87a2414979af9b5
debbie.macrohon@avanade.com <debbie.macrohon@avanade.com>
2024-01-08 01:21:23 +0000

    completed delta lake lab

--- 03 - Delta Lake/DE 3.6L - Load Data Lab.sql
+++ 03 - Delta Lake/DE 3.6L - Load Data Lab.sql
@@ -1,6 +1,6 @@
 -- Databricks notebook source
 -- MAGIC %md-sandbox
--- MAGIC 
+-- MAGIC
 -- MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
 -- MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
 -- MAGIC </div>
@@ -8,12 +8,12 @@
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
 -- MAGIC # Load Data Lab
--- MAGIC 
+-- MAGIC
 -- MAGIC In this lab, you will load data into new and existing Delta tables.
--- MAGIC 
+-- MAGIC
 -- MAGIC ## Learning Objectives
 -- MAGIC By the end of this lab, you should be able to:
 -- MAGIC - Create an empty Delta table with a provided schema
@@ -23,10 +23,10 @@
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
 -- MAGIC ## Run Setup
--- MAGIC 
+-- MAGIC
 -- MAGIC Run the following cell to configure variables and datasets for this lesson.
 
 -- COMMAND ----------
@@ -36,16 +36,16 @@
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
 -- MAGIC ## Data Overview
--- MAGIC 
+-- MAGIC
 -- MAGIC We will work with a sample of raw Kafka data written as JSON files. 
--- MAGIC 
+-- MAGIC
 -- MAGIC Each file contains all records consumed during a 5-second interval, stored with the full Kafka schema as a multiple-record JSON file. 
--- MAGIC 
+-- MAGIC
 -- MAGIC The schema for the table:
--- MAGIC 
+-- MAGIC
 -- MAGIC | field  | type | description |
 -- MAGIC | ------ | ---- | ----------- |
 -- MAGIC | key    | BINARY | The **`user_id`** field is used as the key; this is a unique alphanumeric field that corresponds to session/cookie information |
@@ -63,15 +63,24 @@
 
 -- COMMAND ----------
 
--- TODO
-<FILL_IN>
+
+create table events_raw
+(
+key binary,
+offset long,
+partition integer,
+timestamp long,
+topic string,
+value binary
+
+)
 
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
+-- MAGIC
 -- MAGIC Run the cell below to confirm the table was created correctly.
 
 -- COMMAND ----------
@@ -81,48 +90,49 @@
 -- MAGIC expected_table = lambda: spark.table("events_raw")
 -- MAGIC suite.test_not_none(lambda: expected_table(), "Created the table \"events_raw\"")
 -- MAGIC suite.test_equals(lambda: expected_table().count(), 0, "The table should have 0 records")
--- MAGIC 
+-- MAGIC
 -- MAGIC suite.test_schema_field(lambda: expected_table().schema, "key", "BinaryType")
 -- MAGIC suite.test_schema_field(lambda: expected_table().schema, "offset", "LongType")
 -- MAGIC suite.test_schema_field(lambda: expected_table().schema, "partition", "IntegerType")
 -- MAGIC suite.test_schema_field(lambda: expected_table().schema, "timestamp", "LongType")
 -- MAGIC suite.test_schema_field(lambda: expected_table().schema, "topic", "StringType")
 -- MAGIC suite.test_schema_field(lambda: expected_table().schema, "value", "BinaryType")
--- MAGIC 
+-- MAGIC
 -- MAGIC suite.display_results()
 -- MAGIC assert suite
 
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
+-- MAGIC
 -- MAGIC ## Insert Raw Events Into Delta Table
--- MAGIC 
+-- MAGIC
 -- MAGIC Once the extracted data and Delta table are ready, insert the JSON records from the **`events_json`** table into the new **`events_raw`** Delta table.
 
 -- COMMAND ----------
 
 -- TODO
-<FILL_IN>
+insert into events_raw
+select * from events_json
 
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
 -- MAGIC Manually review the table contents to ensure data was written as expected.
 
 -- COMMAND ----------
 
 -- TODO
-<FILL_IN>
+select * from events_raw
 
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
+-- MAGIC
 -- MAGIC Run the cell below to confirm the data has been loaded correctly.
 
 -- COMMAND ----------
@@ -132,36 +142,38 @@
 -- MAGIC expected_table = lambda: spark.table("events_raw")
 -- MAGIC suite.test_not_none(lambda: expected_table(), "Created the table \"events_raw\"")
 -- MAGIC suite.test_equals(lambda: expected_table().count(), 2252, "The table should have 2252 records")
--- MAGIC 
+-- MAGIC
 -- MAGIC first_five = lambda: [r["timestamp"] for r in expected_table().orderBy(F.col("timestamp").asc()).limit(5).collect()]
 -- MAGIC suite.test_sequence(first_five, [1593879303631, 1593879304224, 1593879305465, 1593879305482, 1593879305746], True, "First 5 values are correct")
--- MAGIC 
+-- MAGIC
 -- MAGIC last_five = lambda: [r["timestamp"] for r in expected_table().orderBy(F.col("timestamp").desc()).limit(5).collect()]
 -- MAGIC suite.test_sequence(last_five, [1593881096290, 1593881095799, 1593881093452, 1593881093394, 1593881092076], True, "Last 5 values are correct")
--- MAGIC 
+-- MAGIC
 -- MAGIC suite.display_results()
 -- MAGIC assert suite.passed
 
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
+-- MAGIC
 -- MAGIC ## Create a Delta Table From Query Results
--- MAGIC 
+-- MAGIC
 -- MAGIC In addition to new events data, let's also load a small lookup table that provides product details that we'll use later in the course.
 -- MAGIC Use a CTAS statement to create a managed Delta table named **`item_lookup`** that extracts data from the parquet directory provided below. 
 
 -- COMMAND ----------
 
 -- TODO
-<FILL_IN> ${da.paths.datasets}/ecommerce/raw/item-lookup
+create or replace table item_lookup
+as 
+select * from parquet.`${da.paths.datasets}/ecommerce/raw/item-lookup`;
 
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
+-- MAGIC
 -- MAGIC Run the cell below to confirm the lookup table has been loaded correctly.
 
 -- COMMAND ----------
@@ -170,18 +182,18 @@
 -- MAGIC suite = DA.tests.new("Validate item_lookup")
 -- MAGIC expected_table = lambda: spark.table("item_lookup")
 -- MAGIC suite.test_not_none(lambda: expected_table(), "Created the table \"item_lookup\"")
--- MAGIC 
+-- MAGIC
 -- MAGIC actual_values = lambda: [r["item_id"] for r in expected_table().collect()]
 -- MAGIC expected_values = ['M_PREM_Q','M_STAN_F','M_PREM_F','M_PREM_T','M_PREM_K','P_DOWN_S','M_STAN_Q','M_STAN_K','M_STAN_T','P_FOAM_S','P_FOAM_K','P_DOWN_K']
 -- MAGIC suite.test_sequence(actual_values, expected_values, False, "Contains the 12 expected item IDs")
--- MAGIC 
+-- MAGIC
 -- MAGIC suite.display_results()
 -- MAGIC assert suite.passed
 
 -- COMMAND ----------
 
 -- MAGIC %md 
--- MAGIC 
+-- MAGIC
 -- MAGIC  
 -- MAGIC Run the following cell to delete the tables and files associated with this lesson.
 
